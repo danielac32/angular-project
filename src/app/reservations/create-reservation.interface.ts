460140e2a0c333e7c54fd4b4bb32b9194f54db09
Earvin Gomez <earvinsantiago.11@gmail.com>
2024-04-09 16:10:33 -0400

    modularization init

--- src/app/reservations/create-reservation.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface CreateReservation {
-    startDate: string;
-    endDate: string;
-    requerimiento: string;
-    cantidad_persona: number;
-    descripcion: string;
-    userId: number;
-    salonId: number;
-}
--- src/app/reservations/status-reserve.interface.ts
+++
@@ -1,7 +0,0 @@
-export enum StatusReserve {
-    PENDING = 'pending',
-    REFUSED = 'refused',
-    ACCEPTED = 'accepted'
-  };
-
-export type StatusReserveTypes = StatusReserve.ACCEPTED | StatusReserve.PENDING | StatusReserve.REFUSED;
